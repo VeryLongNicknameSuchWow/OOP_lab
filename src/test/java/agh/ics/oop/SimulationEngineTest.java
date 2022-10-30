4f99affcdb6cb06e2460479ad5b57467a66af6f0
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2022-10-30 17:19:28 +0100

    integration tests

---
+++ src/test/java/agh/ics/oop/SimulationEngineTest.java
@@ -0,0 +1,27 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class SimulationEngineTest {
+
+    @Test
+    void testCollision() {
+        // two duplicate starting positions, only one of them should create an Animal
+        Vector2d[] startingPositions = {new Vector2d(1, 1), new Vector2d(3, 4), new Vector2d(3, 4)};
+        // last move is impossible, because the position will be occupied
+        MoveDirection[] directions = OptionsParser.parse("f,l,f,f,f,f".split(","));
+        IWorldMap map = new RectangularMap(3, 4);
+        IEngine engine = new SimulationEngine(directions, map, startingPositions);
+
+        Assertions.assertEquals(2, map.getAnimals().length, "map should contain 2 animals");
+        engine.run();
+
+        Vector2d[] finalPositions = new Vector2d[]{new Vector2d(1, 4), new Vector2d(2, 4)};
+        for (Vector2d position : finalPositions) {
+            Assertions.assertNotNull(map.objectAt(position), "there should be an object at " + position);
+        }
+    }
+
+}
